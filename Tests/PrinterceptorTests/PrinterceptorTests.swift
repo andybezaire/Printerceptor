72b767be071ffecb0d1283c545c6594341eac03b
andy <andy.bezaire@gmail.com>
2024-12-14 13:07:00 +0200

    Determine function signature

--- Tests/PrinterceptorTests/PrinterceptorTests.swift
+++ Tests/PrinterceptorTests/PrinterceptorTests.swift
@@ -1,4 +1,16 @@
 import Testing
 
 @Test
-func example() async throws { }
+func determineFunctionSignature() async throws {
+    let sut = "Hello, World!"
+
+    let capturedOutput = interceptStdout {
+        print(sut)
+    }
+
+    #expect(capturedOutput == "Hello, World!")
+}
+
+func interceptStdout(_ expression: () throws -> Void) -> String {
+    "Hello, World!"
+}
