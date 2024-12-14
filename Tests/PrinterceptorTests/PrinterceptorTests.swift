26e3d0f038b07abb25aa2a64e36f556c65950e51
andy <andy.bezaire@gmail.com>
2024-12-14 14:45:00 +0200

    Refactor to own suite

--- Tests/PrinterceptorTests/PrinterceptorTests.swift
+++ Tests/PrinterceptorTests/PrinterceptorTests.swift
@@ -15,15 +15,18 @@ struct PrinterceptorTests {
         #expect(captured == "Hello, World!")
     }
 
-    @Test @MainActor
-    func captureData() async throws {
-        let sut = "Hello, World!"
+    @Suite
+    struct InterceptStdoutTests {
+        @Test @MainActor
+        func captureData() async throws {
+            let sut = "Hello, World!"
 
-        let intercepted: Data = try await interceptStdout {
-            print(sut)
+            let intercepted: Data = try await interceptStdout {
+                print(sut)
+            }
+
+            #expect(intercepted == .init([72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33, 10]))
         }
-
-        #expect(intercepted == .init([72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33, 10]))
     }
 }
 
