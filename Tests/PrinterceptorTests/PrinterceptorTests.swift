71af15f07a2ead86d0af7da1f2c6e1802b60465f
andy <andy.bezaire@gmail.com>
2024-12-14 13:16:56 +0200

    Add documentation

--- Tests/PrinterceptorTests/PrinterceptorTests.swift
+++ Tests/PrinterceptorTests/PrinterceptorTests.swift
@@ -11,6 +11,22 @@ func determineFunctionSignature() async throws {
     #expect(capturedOutput == "Hello, World!")
 }
 
+/// Intercept and return stdout
+///
+/// ```swift
+/// import Testing
+///
+/// @Test func helloWorld() async throws {
+///     let capturedOutput = interceptStdout {
+///         print("Hello, World!")
+///     }
+///
+///     #expect(capturedOutput == "Hello, World!")
+/// }
+/// ```
+///
+/// - Parameter expression: the code that prints to `stdout`
+/// - Returns: string version of `stdout`
 func interceptStdout(_ expression: () throws -> Void) -> String {
     "Hello, World!"
 }
