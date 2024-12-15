3bcd37e5a250b6c5d96f374a357f5ed88ef920a6
andy <andy.bezaire@gmail.com>
2024-12-15 11:35:00 +0200

    Remove task.sleep

--- Sources/Printerceptor/Printerceptor.swift
+++ Sources/Printerceptor/Printerceptor.swift
@@ -36,19 +36,27 @@ internal func interceptStdout(_ expression: () -> Void) async throws -> Data {
     let data = Task {
         var data: Data = .init()
 
+        let semaphore: [UInt8] = [33, 33, 69, 78, 68, 33, 79, 70, 33, 70, 73, 76, 69, 33, 33, 33, 10]
+        var buffer: [UInt8] = []
+
         for try await availableData in intercepted.fileHandleForReading.bytes {
-            data.append(availableData)
+            buffer.append(availableData)
+            if buffer.count > semaphore.count {
+                data.append(buffer.removeFirst())
+
+            }
+
+            if buffer == semaphore { break }
         }
+
+        dup2(restoreForStandardOutput, standardOutput)
+        try intercepted.fileHandleForReading.close()
 
         return data
     }
 
     expression()
-
-    try await Task.sleep(nanoseconds: 1_000_000)
-
-    dup2(restoreForStandardOutput, standardOutput)
-    try intercepted.fileHandleForReading.close()
+    print("!!END!OF!FILE!!!")
 
     return try await data.value
 }
